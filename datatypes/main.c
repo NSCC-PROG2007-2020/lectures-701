a262a94bb1dffa8331134929e7e1f7077ff04f39
Brian Shewan <brian.shewan@nscc.ca>
2020-01-14 10:20:51 -0400

    finished data types lecture

--- datatypes/main.c
+++ datatypes/main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <float.h>
 
 int main() {
 
-    int num;
+    /* standard data types */
+    int num = 123456;
+    float fnum = 3.14159f;
+    char ch = 98;
 
-    num = 123456;
+    printf("range: %i to %i, num = %i\n", INT32_MIN, INT32_MAX, num);
+    printf("range: %g to %g, fnum = %f\n", -FLT_MAX, FLT_MAX, fnum);
+    printf("ch = %c (%i)\n", ch, ch);
 
-    printf("num = %i\n", num);
+    /* special data types */
+    unsigned int unum = 123456;
+    unsigned short int snum = 32768U;
+    long int lnum = 2147483648L;
+    unsigned long int ulnum = 2147483648UL;
+
+    printf("range: %u to %u, unum = %u\n", 0, UINT32_MAX, unum);
+    printf("range: %u to %u, snum = %hu\n", 0, UINT16_MAX, snum);
+    printf("range: %i to %i, lnum = %li\n", INT32_MIN, INT32_MAX, lnum);
 
     return 0;
 }
